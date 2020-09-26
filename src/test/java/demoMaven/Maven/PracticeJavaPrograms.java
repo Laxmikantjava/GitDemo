6872cb7755f464f7d43cb962dfad0aee02d35b94
laxmikant <laxmikant211@gmail.com>
2020-09-26 12:14:59 +0530

    first commit

---
+++ src/test/java/demoMaven/Maven/PracticeJavaPrograms.java
@@ -0,0 +1,22 @@
+package demoMaven.Maven;
+
+public class PracticeJavaPrograms {
+
+	public static void main(String[] args) {
+		
+	int a =0;
+	int b=1;
+	
+	//swap two number with third variable
+
+	
+	for(int i=0;i<10;i++) {
+		int c=a+b;
+		
+	}
+	
+	
+		
+	}
+	
+}
